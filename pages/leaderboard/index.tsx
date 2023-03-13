2a95318e7306402a2b295ebe721dd961beb9132f
Christian Bound <automaticbound@gmail.com>
2023-03-13 13:38:10 +0900

    leaderboard page receiving props. needs styling

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,19 +1,55 @@
+
+import Image from "next/image"
 import { FC } from "react"
 
 
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/leaderboard')
+  const data = await res.json()
+  const { leaderboard } = data
 
-const Leaderboard: FC = () => {
+  return {
+    props: {
+      leaderboard
+    }
 
 
-  return (<>
-    <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1 className="text-4xl font-bold">Leaderboard</h1>
-      <p>TODO: complete.</p>
-      <p>All leaderboard entries should be links to user profile page.</p>
+  }
+}
+
+
+const Leaderboard: FC = (props) => {
+
+  const { leaderboard } = props
+  console.log(leaderboard)
+
+
+
+  return (
+    <div className="grid-cols-1">
+
+      {leaderboard.map((user: object) =>
+        <>
+          <Image width={50} height={50} alt={user.username} src={user.profileImage} />
+          <h1>{user.username}</h1>
+          <h2>{user.score}</h2>
+        </>
+      )}
     </div>
-  </>)
+
+
+
+
+  )
 
 }
 
 
 export default Leaderboard
+
+  // <>
+  //   <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
+  //     <h1 className="text-4xl font-bold">Leaderboard</h1>
+  //     <p>TODO: complete.</p>
+  //   </div>
+  // </>
